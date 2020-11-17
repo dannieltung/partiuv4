02083994d526779b7f7a5c7a0eaf8cc3456e6296
Danniel Tung <dwftung@gmail.com>
2020-11-17 17:56:12 -0300

    routes created

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,20 +5,20 @@ class FavoritesController < ApplicationController
     if @favorite.save
       redirect_to favorit_path(@favorite), notice: 'Favorites created!'
     end
-
-    def destroy
-      @favorite = Favorite.find(params[:id])
-      unless @favorite.user == current_user
-        redirect_to root_path, notice: 'Not allowed to Delete 🤬'
-      end
-      @favorite.destroy
-      redirect_to root_path, notice: 'Favorite Deleted'
-    end
-
-    private
-
-    def favorite_params
-      params.require(:favorite).permit(:spot_id)
-    end
-
   end
+
+  def destroy
+    @favorite = Favorite.find(params[:id])
+    unless @favorite.user == current_user
+      redirect_to root_path, notice: 'Not allowed to Delete 🤬'
+    end
+    @favorite.destroy
+    redirect_to root_path, notice: 'Favorite Deleted'
+  end
+
+  private
+
+  def favorite_params
+    params.require(:favorite).permit(:spot_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-
+  resources :spots, only: [:show, :create, :edit, :update, :destroy] do
+    resources :favorites, only: [:create, :destroy]
+  end
+  resources :users, only: [:show]
 end
