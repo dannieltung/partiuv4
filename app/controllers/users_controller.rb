3c9dc6491400fc77bf415a7852a868eb2b4f5fed
Danniel Tung <dwftung@gmail.com>
2020-11-17 17:33:40 -0300

    finishes users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to root_path, notice: 'Not your account! 🤬'
+    end
   end
 end
