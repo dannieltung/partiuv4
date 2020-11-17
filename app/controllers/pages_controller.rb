7b95ed584e5b1ea5e2b05a946d82b34cabab135f
Danniel Tung <dwftung@gmail.com>
2020-11-17 18:06:19 -0300

    home view refactored

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,14 @@ class PagesController < ApplicationController
 
   # o equivalente ao index
   def home
+    @spot = Spot.new(spot_params)
     @spots = Spot.all
+  end
+
+  private
+
+  def spot_params
+    params.require(:spot).permit(:address, :crowd_congestion, :description, photos: [])
   end
 
 end
