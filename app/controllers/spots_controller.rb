b122ee9d5f49aadf855bdfc011eea267ae2d26d6
Danniel Tung <dwftung@gmail.com>
2020-11-17 17:17:54 -0300

    todays first commit

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -4,16 +4,32 @@ class SpotsController < ApplicationController
   end
 
   def create
-
+    @spot = Spot.new(spot_params)
+    @spot.user == current_user
+    if @spot.save
+      redirect_to spot_path(@spot), notice: 'Spot Created!'
+    else
+      render 'pages/home'
+    end
   end
 
   def edit
+    @spot = Spot.find(params[:id])
+    unless @spot.user == current_user
+      redirect_to root_path, notice: 'Not allowed to Edit 😫'
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def spot_params
+    params.require(:spot).permit(:address, :crowd_congestion, :description, photos: [])
   end
 
 end
