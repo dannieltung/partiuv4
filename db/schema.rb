66271dc44db949226ef2eca6ed52467dcb148f76
Danniel Tung <dwftung@gmail.com>
2020-11-18 18:23:34 -0300

    added fav spot to favorites

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_015122) do
+ActiveRecord::Schema.define(version: 2020_11_18_212218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_11_18_015122) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
+    t.string "fav_spot"
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
