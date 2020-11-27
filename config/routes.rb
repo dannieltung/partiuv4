703703e0886810f7524a510fe1b8b4f5ecf8d445
Danniel Tung <dwftung@gmail.com>
2020-11-26 22:43:31 -0300

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :spots, only: [:new, :create, :show]
-  resources :favorites
   resources :users, only: [:show]
+  resources :spots, only: [:new, :create, :show]
+  resources :favorites, only: [:index, :new, :create]
+  resources :crowdnesses, only: [:new, :create]
 end
---
+++ test/controllers/crowdnesses_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CrowdnessesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
