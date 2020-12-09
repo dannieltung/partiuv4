6b9fe19b6535ae57b77ec5c8d6926d34c5f4c611
Danniel Tung <dwftung@gmail.com>
2020-12-09 18:07:20 -0300

    done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,6 +18,8 @@ import "bootstrap";
 import { initMapbox } from 'plugins/init_mapbox';
 import { showPreview } from './img_preview';
 
+window.$ = $;
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
