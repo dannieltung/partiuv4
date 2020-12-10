fe7fe61ebfe855fff5bb6025ff1a36fe8fee91d3
Danniel Tung <dwftung@gmail.com>
2020-12-10 19:13:41 -0300

    done

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -6,7 +6,7 @@ const mapElement = document.getElementById('map');
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 3000 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 20, duration: 2000 });
 };
 
 const addMarkersToMap = (map, markers) => {
